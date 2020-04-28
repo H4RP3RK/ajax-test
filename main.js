4dc667111121135249a705f73d3a5256f071dfb2
H4RP3RK <kirsty.harper@zoho.com>
2020-04-28 21:06:44 +0000

    pagination

--- main.js
+++ main.js
@@ -1,48 +1,61 @@
-const baseURL = "https://ci-swapi.herokuapp.com/api/"
-
-function getData(type, cb) {
-    let xhr = new XMLHttpRequest();
-
-    xhr.open("GET", baseURL + type + "/");
-    xhr.send();
+function getData(url, cb) {
+    var xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             cb(JSON.parse(this.responseText));
-        };
+        }
     };
-};
+
+    xhr.open("GET", url);
+    xhr.send();
+}
 
 function getTableHeaders(obj) {
-    let tableHeaders = [];
+    var tableHeaders = [];
 
     Object.keys(obj).forEach(function(key) {
         tableHeaders.push(`<td>${key}</td>`);
     });
 
-    return (`<tr>${tableHeaders}</tr>`);
+    return `<tr>${tableHeaders}</tr>`;
 }
 
-function writeToDocument(type) {
-    let tableRows = [];
-    let el = document.getElementById("data");
-    el.innerHTML = "";
+function generatePaginationButtons(next, prev) {
+    if (next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>
+                <button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (next && !prev) {
+        return `<button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (!next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+    }
+}
 
-    getData(type, function(data) {
+function writeToDocument(url) {
+    var tableRows = [];
+    var el = document.getElementById("data");
+
+    getData(url, function(data) {
+        var pagination = "";
+
+        if(data.next || data.previous) {
+            pagination = generatePaginationButtons(data.next, data.previous);
+        }
         data = data.results;
         let tableHeaders = getTableHeaders(data[0]);
 
         data.forEach(function(item) {
-            let dataRow = [];
+            var dataRow = [];
 
             Object.keys(item).forEach(function(key) {
-                let rowData = item[key].toString();
-                let truncatedData = rowData.substring(0, 15)
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15);
                 dataRow.push(`<td>${truncatedData}</td>`);
             });
             tableRows.push(`<tr>${dataRow}</tr>`);
-        })
+        });
 
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
     });
-};
+}
